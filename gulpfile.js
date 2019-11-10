a1816b857e0bba4122aae0917119bbf9d5b81e2f
Hong Baojin <13809735212@163.com>
2019-11-10 21:46:46 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const { src, dest, parallel, watch, series } = require('gulp');
+const sass = require('gulp-sass');
+const gulpCached = require('gulp-cached');
+const autoprefixer = require("gulp-autoprefixer");
+const connect = require('gulp-connect');
+const livereload = require('gulp-livereload');
+const open = require('open');
+
+function html() {
+  return src('src/*.html')
+    .pipe(dest('dist/'))
+    .pipe(livereload());
+}
+
+function css() {
+  return src('src/sass/*.scss')
+    .pipe(gulpCached('src/sass/'))
+    .pipe(sass(
+      {
+        outputStyle: 'expanded',  // 编译成css的标准格式
+        // outputStyle: 'compact',  // 编译成css的一行
+      }
+    ))
+    .pipe(autoprefixer(["last 4 versions"]))
+    .pipe(dest('dist/css'))
+    .pipe(livereload());
+}
+
+function js() {
+  return src('src/js/*.js')
+    .pipe(dest('dist/js'))
+    .pipe(livereload());
+}
+
+// exports.default = parallel(html, css, js);
+
+const watcher = watch(['src/*.html', 'src/sass/*.scss', 'src/js/*.js']);
+
+exports.default = function() {
+  livereload.listen();
+  connect.server({
+    root: 'dist',  // 访问的目录
+    livereload: true,
+    host: '192.168.0.123', // 宿舍ip地址
+    port: 5000,
+  });
+  watch(['src/*.html', 'src/sass/*.scss', 'src/js/*.js'], parallel(html, css, js));
+
+  open('192.168.0.123:5000');
+  
+  // watcher.on(
+  //   'change', 
+  //   parallel(html, css, js)
+  // );
+};
